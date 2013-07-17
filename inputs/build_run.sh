1c574b6b666f216677dd7adaf563f8d20cfb7a1f
Sylvain Boilard <boilard@crans.org>
2013-07-17 12:12:03 +0200

    Track input files.

---
+++ inputs/build_run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cat title > complete_run
+cat world_select >> complete_run
+cat level_select >> complete_run
+cat gameplay_1_1 >> complete_run
+cat level_next >> complete_run
+cat gameplay_1_2 >> complete_run
+cat level_next >> complete_run
+cat gameplay_1_3 >> complete_run
+cat level_next >> complete_run
